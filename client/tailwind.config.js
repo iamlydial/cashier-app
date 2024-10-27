88f930a8603d266e14b51c952f37494acc833bab
iamlydial <lydia.lavecchia@gmail.com>
2024-10-27 12:53:44 +0100

    chore: add GoogleFont DM Sans to html head and tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,17 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      'celticBlue': "#295CD6",
-      'cornflowerBlue': "#698FEA",
-      'arylideYellow': "#ECCD6B",
-      'yankeesBlue': "#202B46",
-      'eerieBlack': "#1D1D1D",
-      'americanSilver': "#D1D0D0",
-      'eerieBlack': "#1D1D1D",
-      'persianRed': "#CF2E2E",
-      'littleGirlPink':'#F8BED4'
+      celticBlue: "#295CD6",
+      cornflowerBlue: "#698FEA",
+      arylideYellow: "#ECCD6B",
+      yankeesBlue: "#202B46",
+      eerieBlack: "#1D1D1D",
+      americanSilver: "#D1D0D0",
+      eerieBlack: "#1D1D1D",
+      persianRed: "#CF2E2E",
+      littleGirlPink: "#F8BED4",
     },
     fontFamily: {
+      sans: ["DM Sans", "sans-serif"],
     },
     extend: {},
   },

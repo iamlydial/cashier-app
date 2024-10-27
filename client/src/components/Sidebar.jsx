3ae50139c0dc70e9feab73b6a351350db8354251
iamlydial <lydia.lavecchia@gmail.com>
2024-10-27 17:51:38 +0100

    feat: add constans folder and sidebar constants

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import DashboardLayout from "../assets/DashboardLayout.svg";
+import { menuItems } from "../constants/sidebarConstants";
+
 const Sidebar = () => {
   return (
     <nav
       data-testid="sidebar"
       className="w-64 bg-yankeesBlue  fixed h-full pt-8"
     >
-      <ul className="flex flex-col items-center border gap-y-16">
-        <li data-testid="home" className="">
-          <div className="flex flex-col items-center justify-center h-32 w-28 rounded-3xl hover:bg-littleGirlPink">
-            <img src={DashboardLayout} className="inline-block w-12 h-12" />
-            <h1 className="text-2xl text-white font-bold">Home</h1>
-          </div>
-        </li>
+      <ul className="flex flex-col items-center gap-y-16">
+        {menuItems.map((item) => (
+          <li data-testid={item.id} className="">
+            <div className="flex flex-col items-center justify-center h-32 w-28 rounded-3xl hover:bg-littleGirlPink">
+              <img src={item.icon} className="inline-block w-12 h-12" />
+              <h1 className="text-2xl text-white font-bold">{item.label}</h1>
+            </div>
+          </li>
+        ))}
       </ul>
     </nav>
   );
---
+++ client/src/constants/sidebarConstants.js
@@ -0,0 +1,27 @@
+import DashboardLayout from "../assets/DashboardLayout.svg";
+import HistoryIcon from "../assets/PaymentHistory.svg";
+import PromosIcon from "../assets/Discount.svg";
+import MenuIcon from "../assets/RestaurantMenu.svg";
+
+export const menuItems = [
+  {
+    id: "home",
+    label: "Home",
+    icon: DashboardLayout,
+  },
+  {
+    id: "menu",
+    label: "Menu",
+    icon: MenuIcon,
+  },
+  {
+    id: "history",
+    label: "History",
+    icon: HistoryIcon,
+  },
+  {
+    id: "promos",
+    label: "Promos",
+    icon: PromosIcon,
+  },
+];

28845108ea30dd182b49ebbfc07df1ff47d0a747
iamlydial <lydia.lavecchia@gmail.com>
2024-10-27 20:13:53 +0100

    feat: add overlay to add product to cart

--- client/src/components/centralSection/ProductCard.jsx
+++ client/src/components/centralSection/ProductCard.jsx
@@ -5,9 +5,12 @@ const ProductCard = () => {
   return (
     <div
       data-testid="product"
-      className=" flex flex-col w-full bg-lightSilver rounded-3xl h-auto p-4 gap-y-4"
+      className="relative flex flex-col w-full bg-lightSilver rounded-3xl h-auto p-4 gap-y-4 overflow-hidden"
     >
-      <img data-testid="product-image" src={Coffee} alt="" />
+      {/* Product Image */}
+      <img data-testid="product-image" src={Coffee} alt="Coffee" />
+
+      {/* Product Info */}
       <div className="flex flex-row w-full justify-between">
         <h2
           data-testid="product-title"
@@ -18,6 +21,19 @@ const ProductCard = () => {
         <p data-testid="product-price" className="text-eerieBlack">
           $11.33
         </p>
+      </div>
+
+      {/* Hidden Overlay - Slides up on hover */}
+      <div className="absolute bottom-0 left-0 right-0 bg-persianRed bg-opacity-75 text-white p-4 flex flex-col justify-center items-center space-y-2 opacity-0 hover:opacity-100 transition-opacity duration-300 h-full">
+        <div className="flex flex-row w-6/12 h-10  align-middle justify-between border">
+          <button className="bg-white h-full w-4 hover:bg-persianRed-50 text-persianRed font-bold py-2 px-4 rounded-lg">
+            +
+          </button>
+          <p className="font-3xl">1</p>
+          <button className="bg-white h-full w-4 hover:bg-persianRed-50 text-white font-bold py-2 px-4 rounded-lg">
+            -
+          </button>
+        </div>
       </div>
     </div>
   );

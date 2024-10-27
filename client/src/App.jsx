727fc208ba102b18dc10d038212ab9869db51479
iamlydial <lydia.lavecchia@gmail.com>
2024-10-27 13:30:55 +0100

    feat: add top bar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { TopBar } from "./components/TopBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      {/* <p className="font-sans text-littleGirlPink">Test </p> */}
+      <TopBar />
     </>
   );
 }

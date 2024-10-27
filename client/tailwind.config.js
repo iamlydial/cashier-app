b439960b0cba8c4dc7ca4333756f454cea120544
iamlydial <lydia.lavecchia@gmail.com>
2024-10-27 12:41:10 +0100

    chore: add tailwind theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      celticBlue: "#295CD6",
+      'cornflowerBlue': "#698FEA",
+      'arylideYellow': "#ECCD6B",
+      'yankeesBlue': "#202B46",
+      'eerieBlack': "#1D1D1D",
+      'americanSilver': "#D1D0D0",
+      'eerieBlack': "#1D1D1D",
+      'persianRed': "#CF2E2E",
+      'littleGirlPink':'#F8BED4'
+    },
+    fontFamily: {},
     extend: {},
   },
   plugins: [],
-}
+};

a3fa61cb13328cfd2b7056754a77e7a785f01d7c
iamlydial <lydia.lavecchia@gmail.com>
2024-10-27 18:14:37 +0100

    feat: complete the UI for the sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,6 @@ import { TopBar } from "./components/TopBar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <TopBar />
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { menuItems } from "../constants/sidebarConstants";
 
-const Sidebar = () => {
+const Sidebar = ({ sidebarToggle }) => {
   return (
-    <nav
-      data-testid="sidebar"
-      className="w-64 bg-yankeesBlue  fixed h-full pt-8"
-    >
-      <ul className="flex flex-col items-center gap-y-16">
-        {menuItems.map((item) => (
-          <li data-testid={item.id} className="">
-            <div className="flex flex-col items-center justify-center h-32 w-28 rounded-3xl hover:bg-littleGirlPink">
-              <img src={item.icon} className="inline-block w-12 h-12" />
-              <h1 className="text-2xl text-white font-bold">{item.label}</h1>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div>
+      <nav
+        data-testid="sidebar"
+        className={` ${
+          sidebarToggle ? "hidden" : "block"
+        } w-64 bg-yankeesBlue  fixed h-full pt-8`}
+      >
+        <div>
+          <ul className="flex flex-col items-center gap-y-16">
+            {menuItems.map((item) => (
+              <li data-testid={item.id} className="">
+                <div className="flex flex-col items-center justify-center h-32 w-28 rounded-3xl hover:bg-littleGirlPink">
+                  <img src={item.icon} className="inline-block w-12 h-12" />
+                  <h1 className="text-2xl text-white font-bold">
+                    {item.label}
+                  </h1>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        
+      </nav>
+    </div>
   );
 };
 

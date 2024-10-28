c08f006f1afb884bfe08c2fb8a503beaf2371536
iamlydial <lydia.lavecchia@gmail.com>
2024-10-28 18:55:36 +0100

    fix: add to basket overlay

--- client/src/components/centralSection/ProductCard.jsx
+++ client/src/components/centralSection/ProductCard.jsx
@@ -25,13 +25,13 @@ const ProductCard = () => {
 
       {/* Hidden Overlay - Slides up on hover */}
       <div className="absolute bottom-0 left-0 right-0 bg-persianRed bg-opacity-75 text-white p-4 flex flex-col justify-center items-center space-y-2 opacity-0 hover:opacity-100 transition-opacity duration-300 h-full">
-        <div className="flex flex-row w-6/12 h-10  align-middle justify-between border">
-          <button className="bg-white h-full w-4 hover:bg-persianRed-50 text-persianRed font-bold py-2 px-4 rounded-lg">
-            +
+        <div className="flex flex-row w-6/12 h-10 items-center justify-between">
+        <button className="bg-white h-full w-10 hover:bg-persianRed-50 text-persianRed font-bold text-3xl flex justify-center items-center rounded-lg">
+          +  
           </button>
           <p className="font-3xl">1</p>
-          <button className="bg-white h-full w-4 hover:bg-persianRed-50 text-white font-bold py-2 px-4 rounded-lg">
-            -
+          <button className="bg-white h-full w-10 hover:bg-persianRed-50 text-persianRed font-bold text-3xl flex justify-center items-center rounded-lg">
+          -
           </button>
         </div>
       </div>

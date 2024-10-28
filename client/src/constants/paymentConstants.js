65f541da00f66a58ceefd7584cbdfedbaadc3eae
iamlydial <lydia.lavecchia@gmail.com>
2024-10-28 19:33:20 +0100

    feat: add payment constant with icons

---
+++ client/src/constants/paymentConstants.js
@@ -0,0 +1,22 @@
+// import AllProducts from "../assets/AllProducts.svg";
+import Cash from "../assets/Cash.svg";
+import Card from "../assets/Card.svg";
+import Wallet from "../assets/Wallet.svg";
+
+export const paymentItems = [
+  {
+    id: "cash",
+    label: "Cash",
+    icon: Cash,
+  },
+  {
+    id: "card",
+    label: "Card",
+    icon: Card,
+  },
+  {
+    id: "wallet",
+    label: "Wallet",
+    icon: Wallet,
+  },
+];

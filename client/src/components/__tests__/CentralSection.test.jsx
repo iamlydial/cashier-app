a502394e0b4f0544b72ef65fd278c9055bcd6706
iamlydial <lydia.lavecchia@gmail.com>
2024-10-29 12:28:54 +0100

    feat: revise CentralSection test to verify products are retrieved correctly

--- client/src/components/__tests__/CentralSection.test.jsx
+++ client/src/components/__tests__/CentralSection.test.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-import { describe, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, it, vi } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 
 import { expect } from "vitest";
-import CentralSection from "../centralSection/centralSection";
+import CentralSection from "../centralSection/CentralSection.jsx";
 
-describe("CenralSection", () => {
+global.fetch = vi.fn()
+
+describe("CentralSection", () => {
+
+  // clean the mock data 
+  beforeEach(()=>{
+    fetch.mockClear();
+  })
+
+
+  it("render products from server API V1", async() =>{
+    const mockProducts = [
+      { id: 1, name: "Coffee", price: 11.33, imageUrl: "coffee.png" },
+      { id: 2, name: "Green Tea", price: 8.99, imageUrl: "tea.png" },
+    ];
+
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async ()=> mockProducts
+    })
+    render(<CentralSection />);
+    await waitFor(() => {
+      expect(screen.getByText("Coffee")).toBeInTheDocument();
+      expect(screen.getByText("Tea")).toBeInTheDocument();
+    });
+  })
+
+  
+
+
   it("it renders central section with all components", () => {
     render(<CentralSection />);
     screen.debug();

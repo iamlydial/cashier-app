1d677a889c380c7952b3a665f7dce859d7fc73fd
iamlydial <lydia.lavecchia@gmail.com>
2024-10-29 15:17:05 +0100

    feat: add overlay button functionality

--- client/src/components/centralSection/ProductCard.jsx
+++ client/src/components/centralSection/ProductCard.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Coffee from "../../assets/product_images/coffee.png";
 
 const ProductCard = ({product}) => {
+const [quantity, setQuantity] = useState(1)
+
+const handleAdd = () =>{
+  const productWithQuantity = {
+    ...product,
+    quantity: quantity,
+  };
+}
+
+const handleRemove= () =>{
+  if(quantity >0){
+    setQuantity(quantity -1)
+  }
+}
+
   return (
     <div
       data-testid="product"
@@ -26,12 +41,20 @@ const ProductCard = ({product}) => {
       {/* Hidden Overlay - Slides up on hover */}
       <div className="absolute bottom-0 left-0 right-0 bg-persianRed bg-opacity-75 text-white p-4 flex flex-col justify-center items-center space-y-2 opacity-0 hover:opacity-100 transition-opacity duration-300 h-full">
         <div className="flex flex-row w-6/12 h-10 items-center justify-between">
-        <button className="bg-white h-full w-10 hover:bg-persianRed-50 text-persianRed font-bold text-3xl flex justify-center items-center rounded-lg">
-          +  
+        <button 
+         onClick={() => {
+          
+          handleRemove();}}
+        className="bg-white h-full w-10 hover:bg-persianRed-50 text-persianRed font-bold text-3xl flex justify-center items-center rounded-lg">
+          - 
           </button>
-          <p className="font-3xl">1</p>
-          <button className="bg-white h-full w-10 hover:bg-persianRed-50 text-persianRed font-bold text-3xl flex justify-center items-center rounded-lg">
-          -
+          <p className="font-3xl">{quantity}</p>
+          <button
+           onClick={() => {
+            setQuantity(quantity + 1); 
+            handleAdd();}} 
+          className="bg-white h-full w-10 hover:bg-persianRed-50 text-persianRed font-bold text-3xl flex justify-center items-center rounded-lg">
+          +
           </button>
         </div>
       </div>
